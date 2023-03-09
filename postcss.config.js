f36242eb93ea6312f88a28b337a218c89f6559ca
arifmmdv <arif.mmdv@gmail.com>
2023-03-09 21:12:54 +0200

    PostCss Configuration Applied, Initial Styles are created

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     plugins: {
+        'postcss-nesting': { /* plugin options */ },
+        'postcss-import': {},
+        'tailwindcss/nesting': {},
         tailwindcss: {},
         autoprefixer: {},
     },
